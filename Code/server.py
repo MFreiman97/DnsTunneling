0c3ff920c8165e5041be24ae6a2f935246c597d1
Matanya Freiman <mfreiman97@gmail.com>
2019-06-10 17:25:45 +0300

    Function of writing to a text File called "Output.txt" Function of Sniffing-we need to work with the "queries_liste"

--- Code/server.py
+++ Code/server.py
@@ -19,7 +19,6 @@ def decode_from_base32(enc_mes):
     enc_mes = enc_mes.replace('.', '')
     b = bytes(enc_mes, 'utf-8')
     mes = base64.b32decode(b)
-    return mes
     b = str(mes, 'utf-8')
     return b
 
